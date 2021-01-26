5c0a67663bb37ea5cef655683d74f628a1aae462
cristiangar0398 <cristianagr1998@gmail.com>
2021-01-25 22:27:37 -0500

    subiendo primer archivo

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed',{
+    strings: [
+     ,' <i>Diseño Web </i>'
+     ,' <i>Tiendas Virtuales </i>' 
+     , '<i>Mantenimiento </i>'] ,
+
+     typeSpeed : 75 ,
+     startDelay : 2000 ,
+     backSpeed : 75,
+     backDelay : 1200 ,
+     loop : true ,
+     loopCount : false,
+     showCursor : true ,
+     cursorChar : '/',
+
+
+
+});
